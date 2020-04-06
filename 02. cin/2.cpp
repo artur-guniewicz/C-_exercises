23903de4b18c1ba9a1c1e393d99082eade732b75
Artur <52582312+artur-guniewicz@users.noreply.github.com>
2020-04-06 20:37:43 +0200

    Add files via upload

---
+++ 02. cin/2.cpp
@@ -0,0 +1,21 @@
+/* Wczytywanie danych */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+
+    cout << "Podaj liczbę całkowitą (a): ";
+    cin >> a;
+    cout << "Podaj liczbę zmiennoprzecinkową (b): ";
+    cin >> b;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+
+    return 0;
+}
